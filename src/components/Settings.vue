06f729f447281c109408a9919389dc629970061d
Conrad <conrad.heidebrecht@gmail.com>
2018-11-11 08:41:56 -0500

    added settings

---
+++ src/components/Settings.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-content app>
+    <v-container align-content={{ true }}>
+      <v-radio-group v-model='color'>
+        <v-radio
+          v-for='(label, mode) of ColorMode'
+          :key='mode'
+          :label='label'
+          :value='label'
+          color='purple'
+          background-color='pink'
+        />
+      </v-radio-group>
+    </v-container>
+  </v-content>
+</template>
+
+
+<script>
+import { mapState, mapMutations, mapGetters } from 'vuex'
+import { ColorMode } from '@/util/Color'
+
+export default {
+  name: 'Settings',
+  data() {
+    return {
+      ColorMode
+    }
+  },
+  computed: {
+    ...mapState([
+      'colorMode'
+    ]),
+    color: {
+      get() { console.log(this.colorMode); return this.colorMode },
+      set(v) {
+        this.setColorMode(v)
+        return this.colorMode
+      }
+    }
+  },
+  methods: {
+    ...mapMutations([
+      'setColorMode'
+    ])
+  }
+}
+</script>
+
+<style  lang='stylus' scoped>
+@import '../styles/themes.styl'
+
+
+
+</style>
